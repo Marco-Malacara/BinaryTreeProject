edf585062ef70c5165f212491474bbf0ee18b94f
Marco-Malacara <marco.malacara7@gmail.com>
2018-08-10 11:51:33 -0500

    set up default values for data, value, left, right

--- BinarySearchTree/Node.cs
+++ BinarySearchTree/Node.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,7 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
-    class Program
+    class BinaryTree
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
