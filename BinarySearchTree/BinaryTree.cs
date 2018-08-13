f8e0ede48e9c0df979c60ed773dcb096f1ab11c1
Marco-Malacara <marco.malacara7@gmail.com>
2018-08-13 14:06:15 -0500

    fixed code so all logic is in binary tree class and only data is in node class

--- BinarySearchTree/BinaryTree.cs
+++ BinarySearchTree/BinaryTree.cs
@@ -9,10 +9,14 @@ namespace BinarySearchTree
     class BinaryTree
     {
         public Node root;
+        private int movedLeft;
+        private int movedRight;
+
         public BinaryTree()
         {
             root = null;
-            count = 0;
+            movedLeft = 0;
+            movedRight = 0;
         }
 
         public bool IsEmpty()
@@ -28,13 +32,78 @@ namespace BinarySearchTree
             }
             else
             {
-                root.SetNode(root, num);
+                SetNode(root, num);
+            }
+        }
+
+        public Node SetNode(Node node, int input)
+        {
+            if (node == null)
+            {
+                return node = new Node(input);
+            }
+            else if (node.data < input)
+            {
+                if (node.right == null)
+                {
+                    node.right = new Node(input);
+                    return node.right;
+                }
+                else
+                {
+                    return SetNode(node.right, input);
+                }
+
+            }
+            else if (node.data > input)
+            {
+                if (node.left == null)
+                {
+                    node.left = new Node(input);
+                    return node.left;
+                }
+                else
+                {
+                    return SetNode(node.left, input);
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Whoops! You broke something....");
             }
         }
 
         public bool Search(int searchNum)
         {
-            return root.Search(root, searchNum);
+            return Search(root, searchNum);
+        }
+
+        public bool Search(Node node, int numSearch)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            else if (node.data == numSearch)
+            {
+                Console.WriteLine("Your search number " + node.data + " was found!");
+                Console.WriteLine("It was " + movedLeft + " to the left" + " and " + movedRight + " to the right!");
+                return true;
+            }
+            else if (node.data < numSearch)
+            {
+                movedRight++;
+                return Search(node.right, numSearch);
+            }
+            else if (node.data > numSearch)
+            {
+                movedLeft++;
+                return Search(node.left, numSearch);
+            }
+            else
+            {
+                throw new ArgumentException("That item is not in this tree!");
+            }
         }
     }
 }
--- BinarySearchTree/Node.cs
+++ BinarySearchTree/Node.cs
@@ -11,81 +11,12 @@ namespace BinarySearchTree
         public int data;
         public Node left;
         public Node right;
-        private int movedLeft;
-        private int movedRight;
 
         public Node(int value)
         {
             data = value;
             left = null;
             right = null;
-            movedLeft = 0;
-            movedRight = 0;
-        }
-
-        public Node SetNode (Node node, int input)
-        {
-            if (node == null)
-            {
-                return node = new Node(input);
-            }
-            else if (node.data < input)
-            {
-                if(node.right == null)
-                {
-                    node.right = new Node(input);
-                    return node.right;
-                }
-                else
-                {
-                    return SetNode(node.right, input);
-                }
-                
-            }
-            else if (node.data > input)
-            {
-                if (node.left == null)
-                {
-                    node.left = new Node(input);
-                    return node.left;
-                }
-                else
-                {
-                    return SetNode(node.left, input);
-                }
-            }
-            else
-            {
-                throw new System.ArgumentException("Whoops! You broke something....");
-            }
-        }
-
-        public bool Search(Node node, int numSearch)
-        {
-            if (node == null)
-            {
-                return false;
-            }
-            else if (node.data == numSearch)
-            {
-                Console.WriteLine("Your search number " + node.data + " was found!");
-                Console.WriteLine("It was " + movedLeft +  " to the left" + " and " + movedRight + " to the right!");
-                return true;
-            }
-            else if (node.data < numSearch)
-            {
-                movedRight++;
-                return Search(node.right, numSearch);
-            }
-            else if (node.data > numSearch)
-            {
-                movedLeft++;
-                return Search(node.left, numSearch);
-            }
-            else
-            {
-                throw new System.ArgumentException("That item is not in this tree!");
-            }
         }
     }
 }
