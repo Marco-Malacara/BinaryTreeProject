4ecc25d196bb82d06f8567d1e90f717dd0741a10
Marco-Malacara <marco.malacara7@gmail.com>
2018-08-12 22:24:26 -0500

    made a node class

--- BinarySearchTree/BinaryTree.cs
+++ BinarySearchTree/BinaryTree.cs
@@ -8,24 +8,15 @@ namespace BinarySearchTree
 {
     class BinaryTree
     {
-        private int data;
-        private int root;
-        private int value;
-        private BinaryTree left;
-        private BinaryTree right;
-
+        Node rootNode = new Node();
         public BinaryTree()
         {
-            data = 0;
-            root = 0;
-            value = 0;
-            left = null;
-            right = null;
+           
         }
 
-        public void Add(BinaryTree node, int input)
+        public void Add(Node node, int input)
         {
-            if (node.value == 0)
+            if (node = 0)
             {
                 node = new BinaryTree();
                 node.value = input;
--- BinarySearchTree/Node.cs
+++ BinarySearchTree/Node.cs
@@ -8,5 +8,27 @@ namespace BinarySearchTree
 {
     class Node
     {
+        public int data;
+        public Node left;
+        public Node right;
+
+        public Node(int value)
+        {
+            data = 0;
+            left = null;
+            right = null;
+        }
+
+        public void InputData(Node node, int input)
+        {
+            if (node == null)
+            {
+                node = new Node(input);
+            }
+            else if (node.data < input)
+            {
+                
+            }
+        }
     }
 }
